c53a1a0e7a872216a5d87170f6e4da9a1336183a
Itserge1 <realitserge@gmail.com>
2023-05-27 16:25:57 -0400

    Update frontend

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,8 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My App: {grettings.msg}</h1>
+      <h1>Deploy</h1>
+      <h2>My App: {grettings.msg}</h2>
       <p>{grettings.results[0]}</p>
     </div>
   );
